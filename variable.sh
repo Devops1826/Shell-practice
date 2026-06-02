ea44e7a41a3059d5f286d9a2bb2eecac40abcd69
Devops1826 <guvi2420@gmail.com>
2026-06-02 07:09:43 +0530

    commandline arguments

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+#command line arguments
+echo "$PERSON1: Hey $PERSON2, We are going blast you!"
+echo "$PERSON2: Yes, Please You are welcome"
+echo "$PERSON1: Hey $PERSON2 Iam Serious"
+echo "$PERSON2: Hey $PERSON1, We are also Serious"
+
+
