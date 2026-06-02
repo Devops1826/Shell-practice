c00e6fb15c81ff8d277eb7b98b9e232eaf12585c
Devops1826 <guvi2420@gmail.com>
2026-06-02 11:43:09 +0530

    Array

---
+++ Array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("RRR" "PUSPHA" "PEDDI")
+
+echo"Movies are:${MOVIES[@]}" #IT PRINTS ALL MOVIES
+echo"First Movie is:${MOVIES[0]}"
+echo"Second Movie is:${MOVIES[1]}"
+echo"Third Movie is:${MOVIES[2]}"
+
