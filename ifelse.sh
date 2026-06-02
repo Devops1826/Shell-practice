e1e42b46926280a7eaa983c50e2df19f81517fbb
Devops1826 <guvi2420@gmail.com>
2026-06-02 12:14:50 +0530

    if-else

---
+++ ifelse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if[  $NUMBER -ge 20]; then
+    echo"Given Number $NUMBER is greater than or equal to 20"
+else
+    echo"Given Number $NUMBER is Less than 20"
+
+fi
+
+
