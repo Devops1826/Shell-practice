faf4e990296002c34b3733e2f72bcc3292d305a1
Devops1826 <guvi2420@gmail.com>
2026-06-02 06:47:06 +0530

    Conversation code

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,14 @@
 #!bin/bash
 
+PERSON1=Trump
+PERSON2=Iran
+
 echo "Trump: Hey Iran, We are going blast you!"
 echo "Iran: Yes, Please You are welcome"
 echo " Trumph: Hey Iran Iam Serious"
 echo " Iran: Hey Trump, We are also Serious"
+
+echo "$PERSON1: Hey $PERSON2, We are going blast you!"
+echo "$PERSON2: Yes, Please You are welcome"
+echo "$PERSON1: Hey $PERSON2 Iam Serious"
+echo "$PERSON2: Hey $PERSON1, We are also Serious"
