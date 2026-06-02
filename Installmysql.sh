4bc86f1fa9566f9d6ee992e9e96af689125f45de
Devops1826 <guvi2420@gmail.com>
2026-06-02 15:55:21 +0530

    Install Mysql and Nginx steps

---
+++ Installmysql.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+#check root access or Not
+
+if[  $USERID -ne 0 ]; then
+    echo"Please run this script with root access"
+    exit 1
+fi
+
+#echo "Iam continuing......"
+
+dnf list installed mysql
+
+if[  $? -eq 0]; then
+    echo"MySQL is already Installed...SKIPPING"
+else
+    echo"Installing MySQL"
+    dnf install mysql -y
+    
+    if[ $? -ne 0]; then
+        echo"Installing MySQL is... FAILED"
+        exit 1
+    else
+        echo"Installing MySQL is... SUCCESS"
+    fi
+fi
+
+
+dnf list installed nginx
+
+if[  $? -eq 0]; then
+    echo"nginx is already Installed...SKIPPING"
+else
+    echo"Installing nginxL"
+    dnf install nginx -y
+    
+    if[ $? -ne 0]; then
+        echo"Installing nginx is... FAILED"
+        exit 1
+    else
+        echo"Installing nginx is... SUCCESS"
+    fi
+fi
+
+
