49a3464c35692db81b735c0d95b71902ea1bf8e0
Devops1826 <guvi2420@gmail.com>
2026-06-02 10:01:10 +0530

    UserInput

---
+++ Userinput.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo"Please enter your name:"
+
+read USER_NAME 
+
+echo"User Name is: $USER_NAME"
+
+echo "Please Enter your Password:"
+
+read -s PASSWORD
+
+echo "Password is: $PASSWORD"
+
