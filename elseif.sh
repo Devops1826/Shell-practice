db6418e7f7aeaca63a39cbd2699f0e7ecd1aa7f5
Devops1826 <guvi2420@gmail.com>
2026-06-02 12:16:58 +0530

    elif

---
+++ elseif.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if[  $NUMBER -gt 20]; then
+    echo"Given Number $NUMBER is greater than  to 20"
+elif
+    echo"Given Number $NUMBER is equal  to 20"
+
+else
+    echo"Given Number $NUMBER is Less than 20"
+
+fi
