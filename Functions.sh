f0ba84f06871d5c404582fd083f174f65ebf237b
Devops1826 <guvi2420@gmail.com>
2026-06-02 16:06:09 +0530

    Using Functions Mysql and Nginx steps

---
+++ Functions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+#check root access or Not
+
+if[  $USERID -ne 0 ]; then
+    echo"Please run this script with root access"
+    exit 1
+fi
+
+#first arg -> What are you trying to install
+#second arg -> exit code
+VALIDATE(){
+
+    if[ $2 -ne 0]; then
+        echo"Installing $1 is... FAILED"
+        exit 1
+    else
+        echo"Installing $1 is... SUCCESS"
+    fi
+
+}
+#echo "Iam continuing......"
+
+dnf list installed mysql
+if[  $? -eq 0]; then
+    echo"MySQL is already Installed...SKIPPING"
+else
+    echo"Installing MySQL"
+    dnf install mysql -y
+    VALIDATE MySQL $?     
+fi
+
+
+dnf list installed nginx
+if[  $? -eq 0]; then
+    echo"nginx is already Installed...SKIPPING"
+else
+    echo"Installing nginxL"
+    dnf install nginx -y
+    VALIDATE MySQL $?
+fi
+
+
