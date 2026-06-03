be7d6945ada46a1a6272bdbf9422afa45e3be8cc
Devops1826 <guvi2420@gmail.com>
2026-06-03 16:01:55 +0530

    Trap Unexcepted Error findining

---
+++ Loops/Set1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" 
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+#Color Codes
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+trap 'echo "error at $LINENO", command: $BASH_COMMAND"'
+#check root access or Not
+if[  $USERID -ne 0 ]; then
+    echo"Please run this script with root access"
+    exit 1
+fi
+
+
+ for package in $@
+ do
+    echo"$TIMESTAMP [INFO] Installiing $package"
+    dnf install $package &>> $LOGS_FILE
+    if[ $2 -ne 0]; then
+        dnf install $package -y &>> $LOGS_FILE
+        VALIDATE "Installing $package" $?
+    else
+        echo -e "$TIMESTAMP [INFO] $package already installed .... $Y SKIPPING $N"
+    fi
+done
+
+
+
