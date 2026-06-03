e71c82cc280b9914bebaa1a77fe2ceaad2934717
Devops1826 <guvi2420@gmail.com>
2026-06-03 11:12:05 +0530

    Tee command

---
+++ log.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" #/home/ec2-user/shell-logs/logs.sh.log
+
+
+#check root access or Not
+if[  $USERID -ne 0 ]; then
+    echo"Please run this script with root access"
+    exit 1
+fi
+
+#first arg -> What are you trying to install
+#second arg -> exit code
+VALIDATE(){
+
+    if[ $2 -ne 0]; then
+        echo"Installing $1 is... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo"Installing $1 is... SUCCESS" | tee -a $LOGS_FILE
+    fi
+
+}
+#echo "Iam continuing......"
+
+dnf list installed mysql &>> $LOGS_FILE
+
+if[  $? -eq 0]; then
+    echo"MySQL is already Installed...SKIPPING"
+else
+    echo"Installing MySQL"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySQL $?     
+fi
+
+
+dnf list installed nginx &>> $LOGS_FILE
+if[  $? -eq 0]; then
+    echo"nginx is already Installed...SKIPPING"
+else
+    echo"Installing nginxL"
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE MySQL $?
+fi
+
+
