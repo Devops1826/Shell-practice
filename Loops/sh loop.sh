1f3fac386f7be61ca1eb64a4489970bdde183ef2
Devops1826 <guvi2420@gmail.com>
2026-06-03 12:13:48 +0530

    Installing Packages

---
+++ Loops/sh loop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log" 
+
+#check root access or Not
+if[  $USERID -ne 0 ]; then
+    echo"Please run this script with root access"
+    exit 1
+fi
+
+#first arg -> What are you trying to install
+#second arg -> exit code
+VALIDATE(){
+
+    if[ $2 -ne 0]; then
+        echo"Installing $1 is... FAILED"
+        exit 1
+    else
+        echo"Installing $1 is... SUCCESS"
+    fi
+
+}
+ for package in $@
+ do
+    echo"Instakking $package"
+done
+
+
+
