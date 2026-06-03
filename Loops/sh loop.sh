66565615dbe2fe7bb04d458658ded52940ea11e9
Devops1826 <guvi2420@gmail.com>
2026-06-03 12:32:31 +0530

    Installing Packages

--- Loops/sh loop.sh
+++ Loops/sh loop.sh
@@ -30,7 +30,7 @@ VALIDATE(){
         dnf install $package -y &>> $LOGS_FILE
         VALIDATE "Installing $package" $?
     else
-        echo"$package already installed ....SKIPPING"
+        echo "$TIMESTAMP [INFO] $package already installed ....SKIPPING"
     fi
 done
 
